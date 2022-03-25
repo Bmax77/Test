9d7d60eaa2f54f1a6d81575709eb678be4e4ab26
Бусурин Максим Валерьевич <MVBusurin@sberbank.ru>
2022-03-25 13:48:46 +0300

    v1

--- server/index.js
+++ server/index.js
@@ -3,14 +3,23 @@ document.querySelector("#send_bttn").addEventListener('click', sendBIN);
 
 async function sendBIN(e){
     let bin = document.querySelector("#bin").value;
-    console.info(bin)
-    let response = await fetch(`cards/${bin}`);
-    console.info(response);
-    if(response.status && response.status == 200){
-        let json = await response.json();
-        console.info(json);
+    let txt_area = document.querySelector("#response");
+    if(bin.match(/^\b(?:\d[ -]*?){13,16}\b$/)){
+        bin = bin.replace(/[ -]/g, '');
+        let response = await fetch(`cards/${bin}`);
+        console.info(response);
+        if(response.status && response.status == 200){
+            let json = await response.json();
+            console.info(json);
+            txt_area.value = `Карта ${json[1]} ${json[2]}, Банк ${json[4]}, ${json[7]}`;
+        }else if(response.status && response.status == 500){
+            let text = await response.text();
+            console.info(text);
+            txt_area.value = `Информация не найдена`;
+        }else{
+            console.info(response.text());
+        }
     }else{
-        let text = await response.text();
-        console.info(text);
+        txt_area.value = "Введите номера карты";
     }
 }
--- server/server_flask.py
+++ server/server_flask.py
@@ -1,10 +1,15 @@
 #!/bin/env python3
 import os
 from flask import Flask, render_template, request
+import pathlib
+import re
+import csv
+import json
 
-template_dir = ''
+# template_dir = ''
+data = ()
 
-app = Flask(__name__, template_folder=template_dir, static_url_path='', static_folder=template_dir)
+app = Flask(__name__, template_folder='', static_url_path='', static_folder='')
 
 @app.route('/')
 @app.route('/index.html')
@@ -22,7 +27,6 @@ def stop():
 
 @app.route('/health/readiness')
 def readiness():
-    # return("Hello world")
     resp = app.response_class(response='Ready', status=200, mimetype='text/text')
     return(resp)
 
@@ -32,20 +36,30 @@ def about():
 
 @app.route('/cards/<card_number>', methods=['GET'])
 def card(card_number):
-    card_number = card_number[:6]
-    print(card_number)
-    with open('server/binlist-data.csv') as f:
-        if card_number in f.read():
-            return('{"Bank":' + str(card_number)+ '}')
-        else:
-            return('{"Bank":"hot found"}')
+    card_number = re.match(r'\d{16}', card_number)
+    if card_number is not None:
+        resp = None
+        card_number = card_number.group(0)
+        for row in data:
+            if  row[0] in card_number[:len(row[0])]:
+                print(row)
+                resp = app.response_class(response=json.dumps(row), status=200, mimetype='text/text')
+                break
+            else:
+                resp = app.response_class(response='Not found', status=500, mimetype='text/text')
+        return(resp)
+    else:
+        resp = app.response_class(response='Incorrect data', status=500, mimetype='text/text')
+        return(resp)
 
 
 
-if __name__ == "__main__":
-    template_dir = os.getcwd()
-    print('Main' + template_dir)
-    app.run(host='localhost', port=8080, threaded=True, debug=True)
-else:
-    template_dir = os.getcwd() + '/server'
-    print(template_dir)
+
+# if __name__ == "__main__":
+path = pathlib.Path(__file__).parent.resolve()
+print('Main' + str(path))
+with open(path.joinpath('binlist-data.csv'), newline='') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+print(len(data))
+app.run(host='localhost', port=8080, threaded=True, debug=True)
